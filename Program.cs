84daeed41dffaeabe9d90eb4a328c65a0527c392
Julio <juliobrsouza@outlook.com>
2026-07-11 22:07:33 -0300

    feat: added cors

--- Program.cs
+++ Program.cs
@@ -7,14 +7,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+var allowedOrigins = builder.Configuration
+    .GetSection("AllowedOrigins")
+    .Get<string>();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins(allowedOrigins ?? "")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IMangaRepository, MangaRepository>();
 
 builder.Services.AddHttpClient();
-var app = builder.Build();
 
+var app = builder.Build();
 if (app.Environment.IsDevelopment()) app.MapOpenApi();
+
 app.UseHttpsRedirection();
+app.UseCors();
 app.UseAuthorization();
 
 app.MapControllers();
